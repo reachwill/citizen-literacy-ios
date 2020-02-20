ecfc343ba1e00b5ecee9ee3f8ed95c7d31b4d250
Will Gregory <reachwill@gmail.com>
2020-02-20 14:41:35 +0000

    Upload of Product Discovery Phase of the Citizen Literacy Project

--- google-cloud-test/backend/database/db.js
+++ google-cloud-test/backend/database/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //db: 'mongodb+srv://will:wellinever@cluster0-zsfvp.mongodb.net/test?retryWrites=true&w=majority'
-  db: 'mongodb+srv://will:wellinever@cluster0-zsfvp.mongodb.net/citizen-literacy?retryWrites=true&w=majority'
+  
+  //db: 'mongodb+srv://will:xxx@cluster0-zsfvp.mongodb.net/citizen-literacy?retryWrites=true&w=majority'
 }
 
